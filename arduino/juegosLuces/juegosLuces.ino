d2fbd8e644e588d9f977fdb204e92b9b4e929839
rodrigo_l_rosa <rodrigorosa.LG@gmail.com>
2013-06-29 21:28:30 -0300

    [add] first working version

--- arduino/juegosLuces/juegosLuces.ino
+++ arduino/juegosLuces/juegosLuces.ino
@@ -3,7 +3,7 @@ bla.
  */
 
 // Set up 38kHz IR TX on pin 3
-#define IR_KHZ               38
+#define IR_KHZ               56
 #define TIMER_PWM_PIN        3
 #define TIMER_ENABLE_PWM     (TCCR2A |= _BV(COM2B1))
 #define TIMER_DISABLE_INTR   (TIMSK2 = 0)
@@ -24,7 +24,7 @@ void enableIrTransmitters(int khz) {
 		OCR2A  = pwm_val;
 		OCR2B  = pwm_val / 3;
 
-		// Enable PWM output on pin 3
+		// Enable PWM output on pin 3HIGH
 		TIMER_ENABLE_PWM;
 }
 
@@ -32,10 +32,12 @@ int ledPin = 9;           // LED connected to digital pin 9
 // IR receiver
 int pinIRReceiverOut = 7; // IR receiver - Output
 
-void setup()  { 
+void setup()  {
 		//// Setup IR receiver
 		// Read RX output
 		pinMode(pinIRReceiverOut, INPUT);
+		// Disable pull up resistor
+		digitalWrite(pinIRReceiverOut, LOW);
 
 		//// Notification LED
 		analogWrite(ledPin, 0);
@@ -51,19 +53,22 @@ void setup()  {
 
 #define DEBUG            1
 
+#define LOOP_MS          10 // ms
+#define LOOP_PRINT       50 // print once every LOOP_PRINT*LOOP_MS ms.
+
 #define ON               200  // max led value when turned on
 #define OFF              0    // min led value when turned off
-#define STEP             4    // dimmer inc step size
+#define STEP             30    // dimmer inc step size
 
 // After FADE_OUT_SAMPLES at the setpoint, will turn the output off.
 #define FADE_OUT         0    // enable(1)/disable(0)
-#define FADE_OUT_SAMPLES 800  // samples before fading out
+#define FADE_OUT_SAMPLES 100  // samples before fading out
 
 //// Input filter
 // Will wait for FILTER_SAMPLES stable successive samples before
 // changing the output.
 // Set to 0 to disable filter.
-#define FILTER_SAMPLES 10
+#define FILTER_SAMPLES 3
 
 #define min(a,b)        (a<b?a:b)
 #define max(a,b)        (a>b?a:b)
@@ -78,8 +83,14 @@ int cnt_print  = 0;
 int cnt_steady = 0;
 int val = 0;
 int out = 0;
+int dly_ok = 1;
 
 void loop()  {
+		int
+				t_in  = millis(),
+				t_out = 0,
+				t_dly = 0;
+
 		int rx = digitalRead(pinIRReceiverOut);
 
 		// Noise filter
@@ -116,7 +127,7 @@ void loop()  {
 		val = lim(val+to_add, OFF, ON);
 		analogWrite(ledPin, val);
 
-		if(DEBUG && cnt_print++ > 50) {
+		if(DEBUG && cnt_print++ > LOOP_PRINT) {
 				Serial.print("Rx:");Serial.print(rx);
 				Serial.print("\tFilter:");Serial.print(filter_ok(cnt_filter));
 				Serial.print("\tOut:");Serial.print(out);
@@ -132,6 +143,21 @@ void loop()  {
 		// Update for next loop
 		rx_ = rx;
 
-		// No need to hurry.
-		delay(5);
+		// Calculate wait time for loop of LOOP_MS ms.
+		t_out = millis();
+		t_dly = LOOP_MS - (t_out - t_in);
+		if (t_dly > 0) {
+				if (!dly_ok){
+						Serial.println("DELAY OK");
+						dly_ok = 1;
+				}
+				delay(t_dly);
+		}
+		else {
+				if (dly_ok) {
+						Serial.print("ERROR - DELAY:");
+						Serial.println(t_out-t_in);
+						dly_ok = 0;
+				}
+		}
 }
